44cbde4f36a2f9d79c42746d38c6b177d11a4f51
Rodrigo Almeida <rod.almeida01@gmail.com>
2021-01-12 16:57:30 -0300

    User-details-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
-
+import { UserDetailsComponent } from './user-list/user-details/user-details.component'
  
 
 const routes: Routes = [
-  { path: '', component: UserListComponent }
+  { path: '', component: UserListComponent },
+
+  { path: 'userDetails', component: UserDetailsComponent },
+
 ];
 
 @NgModule({
